5f9cc62bf8f4e13e13dfe96611f14a123ad81a07
jiggijacks <114829766+jiggijacks@users.noreply.github.com>
2025-12-25 14:09:18 +0100

    Delete raffle-bot directory

--- raffle-bot/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///raffle.db", echo=False)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
