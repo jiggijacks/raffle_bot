1546d2b1fe2e2949dc48152fbd44a88c7dd2b247
jiggijacks <114829766+jiggijacks@users.noreply.github.com>
2025-12-28 14:01:37 +0100

    Fix package name and structure

--- Desktop/raffle-bot/models,.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Ticket(Base):
-    __tablename__ = "tickets"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-    reference = Column(String)
